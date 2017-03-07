fbc5883af7922edd442df9c2c2cc351895adae9a
eswaribala <Parameswaribala@gmail.com>
2017-03-07 13:28:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -8,12 +8,41 @@ mainModule.value('Options',['Users','Comments','Albums','Posts']);
 mainModule.controller('MainCtrl',['$scope','Options','DashBoardFactory',
     function($scope,Options,DashBoardFactory)
 {
-    $scope.selectedValue="Users";
+    $scope.selectedValue="";
     $scope.ddOptions=Options;
     $scope.callBack=function()
     {
         console.log($scope.selectedValue);
        var obj = DashBoardFactory.findService($scope.selectedValue);
+        //console.log(obj);
+        angular.forEach(obj, function(res)
+        {
+            //console.log(res);
+            //immediate invocation
+            res().then(function(output)
+            {
+                console.log(output);
+                 switch($scope.selectedValue)
+                 {
+                     case 'Users':
+                         $scope.usersResult=output.data;
+                         break;
+                     case 'Comments':
+                         $scope.commentsResult=output.data;
+                         break;
+                     case 'Albums':
+                         $scope.albumsResult=output.data;
+                         break;
+                     case 'Posts':
+                         $scope.postsResult=output.data;
+                         break;
+                 }
+
+            });
+
+        });
+
+
     }
 
 
