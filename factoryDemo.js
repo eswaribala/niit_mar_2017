4ee978d7d0e804ead9c10d3742b5f5413b4a9277
eswaribala <Parameswaribala@gmail.com>
2017-03-07 12:51:08 +0530

    Add files via upload

--- factoryDemo.js
+++ factoryDemo.js
@@ -3,18 +3,28 @@
  */
 var factoryModule=angular.module('FactoryModule',['ServiceModule']);
 
-factoryModule.factory('DashBoardFactory',[function()
+factoryModule.factory('DashBoardFactory',['$injector',function($injector)
 {
   var obj={};
   obj.findService=function(option)
   {
+      obj={};
       angular.module('ServiceModule')['_invokeQueue'].
           forEach(function(value)
       {
-              console.log(value);
+              //console.log(value[2][0]);
+
+             if(value[2][0]==option+'Service') {
+                 console.log(value[2][0]);
+                 console.log(option);
+                 //console.log('Match found....');
+                obj=$injector.get(value[2][0]);
+
+             }
+
       });
 
-
+      return obj;
   }
  return obj;
 
